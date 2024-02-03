267be6968e72ecc427d53eacdf92c90ee1a1d34a
HYngrid Souza <hyngrid.hss@ufpe.br>
2024-02-03 13:04:33 -0300

    Estruturando

---
+++ main.py
@@ -0,0 +1,9 @@
+from livro import Livro
+from revista import Revista
+from tese import Tese
+from usuario import Usuario
+from emprestimo import Emprestimo
+from multa import Multa
+from lista_espera import ListaEspera
+
+# Restante do código...
